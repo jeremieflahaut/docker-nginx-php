242d17e8f3adc6bcb20b9fba02dd6b7bcb106f1e
jflahaut <30655859+jeremieflahaut@users.noreply.github.com>
2023-03-20 11:22:18 +0100

    Update create-container.sh

--- .deploy/create-container.sh
+++ .deploy/create-container.sh
@@ -82,6 +82,6 @@ echo "0 0 * * * sudo /usr/sbin/logrotate -f /etc/logrotate.conf" >> /tmp/mycron
 echo "* * * * * /usr/bin/php /var/www/html/artisan schedule:run >> /var/log/cron.log 2>&1" >> /tmp/mycron
 crontab -u debian /tmp/mycron
 rm /tmp/mycron
-chown debian:crontab /var/spool/cron/crontabs/debian
+chmod u+s /usr/sbin/cron
 echo "Cron install done."
 
